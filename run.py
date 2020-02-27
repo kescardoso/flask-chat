28bee29244a2ec1071d1ab00edb5f40aa70f0556
Kes Cardoso <kesia.development@gmail.com>
2020-02-27 19:48:23 +0000

    Add the ability to write and read chat from a list and siplay messages to the user

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 messages = []
 
 
 def add_messages(username, message):
+    """Add messages to the 'messages'list"""
     messages.append("{}: {}".format(username, message))
+
+
+def get_all_messages():
+    """Get all of the messages and separate them using a 'br'"""
+    return "<br>".join(messages)
 
 
 @app.route('/')
@@ -18,7 +24,7 @@ def index():
 @app.route('/<username>')
 def user(username):
     """Display chat messages"""
-    return "Welcome, {0}".format(username)
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
 
 @app.route('/<username>/<message>')
